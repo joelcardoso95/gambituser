54bf7c92a599864601c8a3bb171076c2b4d42a2e
joelcardoso95 <joelmarcos.cardoso@aol.com>
2024-03-15 18:10:09 -0300

    Last Commit

--- tools/tools.go
+++ tools/tools.go
@@ -7,6 +7,7 @@ import (
 
 func DateMySQL() string {
 	time := time.Now()
-	return fmt.Sprintf("%d-$02d-%02dT%02d:%02d:%02d", time.Year(), time.Month(), time.Day(), time.Hour(), time.Minute(), time.Second())
+	return fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d",
+		time.Year(), time.Month(), time.Day(), time.Hour(), time.Minute(), time.Second())
 
 }
