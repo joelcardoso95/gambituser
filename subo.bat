18418f4b5e8312cd1c0a4c19a4f870563f902fa2
joelcardoso95 <joelmarcos.cardoso@aol.com>
2024-03-15 16:50:30 -0300

    Last Commit

--- subo.bat
+++ subo.bat
@@ -3,6 +3,7 @@ git commit -m "Last Commit"
 git push
 set GOOS=linux
 set GOARCH=arm64
-go build main.go
+set CGO_ENABLED=0
+go build -o bootstrap main.go
 del main.zip
 tar.exe -a -cf main.zip bootstrap
